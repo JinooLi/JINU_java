d6cd3ba3b7f567edb752c294937b175a939fe601
jinooLi <58159227+jinooLi@users.noreply.github.com>
2019-11-25 14:35:18 +0900

    Revert "error"
    This reverts commit c9a1a9f33d9cdfa074b4b2b42d6dd15e41512afa.

--- src/jinu_java/Main.java
+++ src/jinu_java/Main.java
@@ -3,7 +3,31 @@ package jinu_java;
 public class Main {
 	public static void main(String[] args) {
 		double[][] m1={{3,2,3,2},{1,2,4,5},{7,1,6,4},{2,3,4,5}},m2= {{2,3,4,5},{4,3,5,4},{2,5,3,1},{2,6,4,7}};
-		Matr	mat[y][x] += b.mat[y][x];
+		Matrix a = new Matrix(m1);
+		Matrix b = new Matrix(m2);
+		a.add(b);
+		a.matrixPrint();
+	}
+	
+}
+
+//Annotation
+
+class Matrix {
+	
+	public Matrix(double[][] mat) {
+		this.mat = mat;
+		row = mat.length;
+		column = mat[0].length;
+	}
+	
+	double[][] mat;
+	public int row, column;
+
+	public Matrix add(Matrix b) {
+		for (int y = 0; y < mat.length; y++) {
+			for (int x = 0; x < mat[y].length; x++) {
+				mat[y][x] += b.mat[y][x];
 			}
 		}
 		return this;
